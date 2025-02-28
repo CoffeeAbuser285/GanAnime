fc9a8b6f7bcc3805f2369b244a32f4baa30535e5
unknown <kaionedaniels285@gmail.com>
2025-02-28 00:20:59 -0700

    more changes

--- Source/NeuralNetwork/Gan.py
+++ Source/NeuralNetwork/Gan.py
@@ -3,4 +3,11 @@
 class Gan:
     def __init__(self): 
         print("Looking Good Gan")
+        
+    def InitializeParameters(self):
+        pass
+    
+    def TrainNn(self):
+        pass
+    
     
